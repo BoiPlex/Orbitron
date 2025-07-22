a40b2b85132e01c6aea3dfaf4dca7bfc371ec047
BoiPlex <endplex@gmail.com>
2025-07-22 00:38:09 -0700

    Add click explosions

--- orbitron-game/scripts/explosion_factory.gd
+++ orbitron-game/scripts/explosion_factory.gd
@@ -1,2 +1,13 @@
 class_name ExplosionFactory
 extends Node
+
+@export var explosion_scene: PackedScene
+@export var parent: Node
+
+var _explosion_list: Array[Explosion] = []
+
+func make_explosion(pos: Vector2):
+	var explosion = explosion_scene.instantiate() as Explosion
+	explosion.global_position = pos
+	_explosion_list.push_back(explosion)
+	parent.add_child(explosion)
